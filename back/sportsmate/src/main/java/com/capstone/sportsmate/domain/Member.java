c85efb84a8711d300204a9738de7db38709bba1a
superdevsong <superdevsong@gmail.com>
2022-04-01 19:12:27 +0900

    add member entity and add application yml

--- back/sportsmate/src/main/java/com/capstone/sportsmate/domain/Member.java
+++ back/sportsmate/src/main/java/com/capstone/sportsmate/domain/Member.java
@@ -1,12 +1,28 @@
 package com.capstone.sportsmate.domain;
 
 
-import javax.persistence.Entity;
-import javax.persistence.Id;
+import lombok.Getter;
+import lombok.Setter;
+
+import javax.persistence.*;
+import java.util.Date;
 
 @Entity
+@Table(name = "member")
+@Getter @Setter
 public class Member {
-
     @Id
+    @Column(name="member_id")
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
     private Long id;
+    private String name;
+    private String sex;
+    @Column(name="nick_name")
+    private String nickName;
+    private String password;
+    @Column(name="since_date")
+    private Date sinceDate;
+    @Column(name="birth_date")
+    private Date birthDate;
+    private int credit;
 }
