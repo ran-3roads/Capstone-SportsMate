cdb17f766f617c83fe500a325f165e74920c5b1a
superdevsong <superdevsong@gmail.com>
2022-04-06 18:13:11 +0900

    add loginconnect

--- back/sportsmate/src/main/java/com/capstone/sportsmate/service/MemberService.java
+++ back/sportsmate/src/main/java/com/capstone/sportsmate/service/MemberService.java
@@ -22,12 +22,22 @@ public class MemberService {
         return member.getId();
     }
     private void validateDuplicateMember(Member member) {
-        List<Member> findMembers = memberRepository.findByEmail(member.getEmail());
-        if(!findMembers.isEmpty()){
+        Member findMember = memberRepository.findByEmail(member.getEmail());
+        if(findMember!=null){
             throw new IllegalStateException("이미 존재하는 회원입니다.");
         }
     }
     public Member findOne(Long memberId){
         return memberRepository.findOne(memberId);
+    }
+
+    //login service
+    public Member Login(String memberEmail,String password){
+        Member findMember = memberRepository.findByEmail(memberEmail);
+        if(findMember==null)//find member가 비었다면
+            return null;
+        if(!password.equals(findMember.getPassword()))
+            return null;
+        return findMember;
     }
 }
--- back/sportsmate/src/test/java/com/capstone/sportsmate/controller/MemberControllerTest.java
+++ back/sportsmate/src/test/java/com/capstone/sportsmate/controller/MemberControllerTest.java
@@ -1,7 +1,10 @@
 package com.capstone.sportsmate.controller;
 
+import org.springframework.boot.test.autoconfigure.web.servlet.AutoConfigureMockMvc;
 import org.springframework.boot.test.context.SpringBootTest;
 
 @SpringBootTest
+@AutoConfigureMockMvc
 public class MemberControllerTest {
+
 }
