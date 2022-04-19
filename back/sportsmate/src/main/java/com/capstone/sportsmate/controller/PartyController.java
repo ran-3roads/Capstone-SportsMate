1bf474ea1fa5d8706f95d3ddb56dfb653af7cb44
superdevsong <superdevsong@gmail.com>
2022-04-19 18:35:37 +0900

    add party join

--- back/sportsmate/src/main/java/com/capstone/sportsmate/controller/PartyController.java
+++ back/sportsmate/src/main/java/com/capstone/sportsmate/controller/PartyController.java
@@ -1,5 +1,9 @@
 package com.capstone.sportsmate.controller;
-
+import com.capstone.sportsmate.service.PartyBoardService;
+import com.capstone.sportsmate.web.CommentForm;
+import com.capstone.sportsmate.web.PartyBoardForm;
+import com.capstone.sportsmate.domain.PartyBoard;
+import com.capstone.sportsmate.domain.Comment;
 
 import com.capstone.sportsmate.domain.Party;
 import com.capstone.sportsmate.domain.notice.Notice;
@@ -19,7 +23,8 @@ import java.util.List;
 public class PartyController {
     private final PartyService partyService;
     private final MemberService memberService;
-
+    private final PartyBoardService partyBoardService;
+    
     @GetMapping("/myparty") //일단은 그냥 id 값 받는걸로 함 추후 security 숙지되면 변경할 예정
     public List<Party> myParty(){
         List<Party> parties = partyService.findMyParties(memberService.getMyInfo().getId());
@@ -64,5 +69,48 @@ public class PartyController {
         partyService.mkParty(form,memberService.getMyInfo().getId());
         return "success";
     }
+    // --------------------파티 보드--------------------
+
+    //----------조회----------
+    @GetMapping("/{partyId}/partyboard")//해당파티 리스트 response로 따로 만드는 작업이 필요해보임 작성자와 id 제목만 보낸다던가
+    List<PartyBoard> getPartyBoardList(@PathVariable("partyId") Long partyId){
+        return partyBoardService.getPartyBoardList(partyId);
+    }
+
+    @GetMapping("/{partyId}/partyboard/{partyBoardId}")//게시글 선택시 글 리턴 혹은 변경페이지로 이동시 정보 반환
+    PartyBoard partyBoardOne(@PathVariable("partyBoardId") Long partyBoardId){
+        return partyBoardService.getPartyBoard(partyBoardId);
+    }
+    //----------생성----------
+    @PostMapping("/{partyId}/mkpartyboard")//파티 게시판 작성
+    String createPartyBoard(@RequestBody PartyBoardForm partyBoardForm ,@PathVariable("partyId") Long partyId){
+        partyBoardService.createPartyBoard(partyId, partyBoardForm);
+        return "mkparty";
+    }
+    //----------수정----------
+    //    @GetMapping("/{partyId}/partyboard/{partyBoardId}/modify") //멤버 조회후 수정할 정보 리턴 아마 인증만 해주고 그전 내용 반영은 프론트에 맡겨도 될듯
+    //    @PostMapping("/{partyId}/partyboard/{partyBoardId}/modify") //멤버 조회후 수정할 내용 반환
+    //----------삭제----------
+    //    @PostMapping("/{partyId}/partyboard/{partyBoardId}/delete") //멤버 조회후 삭제
+    //--------------------댓글--------------------
+
+    //----------조회----------
+
+    @GetMapping("/{partyId}/partyboard/{partyBoardId}/comment")//게시글 선택시 게시글에 댓글들 리턴
+    List<Comment> getCommentList(@PathVariable("partyBoardId") Long partyBoardId){
+        return partyBoardService.getCommentList(partyBoardId);
+    }
+
+    //----------생성----------
+    @PostMapping("/{partyId}/partyboard/{partyBoardId}/mkcomment")
+    String createComment(@PathVariable("partyBoardId") Long partyBoardId, CommentForm commentForm) {
+        partyBoardService.createComment(partyBoardId, commentForm);
+        return "mkcomment";
+    }
+    //----------수정----------
+    //    @GetMapping("/{partyId}/partyboard/{partyBoardId}/{commentId}/modify") //멤버 조회후 수정할 댓글 정보 리턴 아마 인증만 해주고 그전 내용 반영은 프론트에 맡겨도 될듯
+    //    @PostMapping("/{partyId}/partyboard/{partyBoardId}/{commentId}/modify") //멤버 조회후 수정할 댓글 내용 반환
+    //----------삭제----------
+    //    @PostMapping("/{partyId}/partyboard/{partyBoardId}/{commentId}/delete") //멤버 조회후 댓글 삭제
 
 }
