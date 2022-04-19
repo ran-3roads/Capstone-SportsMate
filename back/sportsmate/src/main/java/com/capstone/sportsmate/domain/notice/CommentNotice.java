75f1bf921f92c540f287b565543b151849049696
superdevsong <superdevsong@gmail.com>
2022-04-19 15:06:19 +0900

    add notice

---
+++ back/sportsmate/src/main/java/com/capstone/sportsmate/domain/notice/CommentNotice.java
@@ -0,0 +1,10 @@
+package com.capstone.sportsmate.domain.notice;
+
+import javax.persistence.DiscriminatorValue;
+import javax.persistence.Entity;
+
+//@Entity
+//@DiscriminatorValue("C")
+//public class CommentNotice extends Notice{
+//
+//}
