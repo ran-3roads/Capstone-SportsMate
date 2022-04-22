58207da094e879d5f706d9a572ba6553ecb85256
superdevsong <superdevsong@gmail.com>
2022-04-22 16:06:52 +0900

    logout update

--- front/pages/logout.js
+++ front/pages/logout.js
@@ -1,1 +1,29 @@
+import Popup from "../components/custom/popup";
+import { useState } from "react";
+import axios from "axios";
+export default function Logout() {
+    const [popup, setPopup] = useState({open: false, title: "", message: "", callback: false});
+    axios.get("http://localhost:8080/sportsmate/member/logout")
+    .then(function (response) {
+        if(response.status == 200){
+        axios.defaults.headers.common['Authorization'] = null;
+        setPopup({
+            open: true,
+            title: "Confirm",
+            message: "Logout Success!", 
+            callback: function(){
+                document.location.href='/'
+                }
+            });
+        }
+    }).catch(function (error) {
+    //error
+    console.log(error);
+    });
+    return(
+        <div>
+             <Popup open = {popup.open} setPopup = {setPopup} message = {popup.message} title = {popup.title} callback = {popup.callback}/>
+        </div>
+    );
+}
 
