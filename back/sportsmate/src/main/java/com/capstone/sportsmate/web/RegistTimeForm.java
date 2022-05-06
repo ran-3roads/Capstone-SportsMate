3ab1d4035e7d983546cf72f42464cf6e88a6f5f6
ran-3roads <ckstjd9735@gmail.com>
2022-05-07 00:52:33 +0900

    schedule edit

--- back/sportsmate/src/main/java/com/capstone/sportsmate/web/RegistTimeForm.java
+++ back/sportsmate/src/main/java/com/capstone/sportsmate/web/RegistTimeForm.java
@@ -4,11 +4,13 @@ package com.capstone.sportsmate.web;
 import com.fasterxml.jackson.annotation.JsonFormat;
 import lombok.AllArgsConstructor;
 import lombok.Getter;
+import lombok.NoArgsConstructor;
 
 import java.time.LocalDate;
 
 @Getter
 @AllArgsConstructor
+@NoArgsConstructor
 public class RegistTimeForm {
 
     private Long arenaId;
