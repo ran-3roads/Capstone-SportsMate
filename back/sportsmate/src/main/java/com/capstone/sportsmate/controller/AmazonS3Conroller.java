8a0447ffea3d15ed90a5f8ccc63b89eae195bf4c
superdevsong <superdevsong@gamil.com>
2022-05-11 17:35:29 +0900

    restrefactoring

--- back/sportsmate/src/main/java/com/capstone/sportsmate/controller/AmazonS3Conroller.java
+++ back/sportsmate/src/main/java/com/capstone/sportsmate/controller/AmazonS3Conroller.java
@@ -19,20 +19,20 @@ public class AmazonS3Conroller {
      * Amazon S3에 파일 업로드
      * @return 성공 시 200 Success와 함께 업로드 된 파일의 파일명 리스트 반환
      */
-    @PostMapping("/image")
+    @PostMapping("/image") //이미지 삽입 수정
     public ResponseEntity<List<String>> uploadFile(@RequestPart List<MultipartFile> multipartFile,
                                                         @RequestParam Long id,
                                                         @RequestParam ImageCategory imageCategory) {//멤버의 경우 필요가 없으니 -1을 부탁한다.
         return ResponseEntity.ok(awsS3Service.uploadFile(multipartFile,id,imageCategory));
     }
-    @PostMapping("/public/signupimage")
+    @PostMapping("/public/signupimage")//멤버 가입할때 이미지 삽입
     public ResponseEntity<List<String>> uploadSingupFile(@RequestPart List<MultipartFile> multipartFile,
                                                    @RequestParam String email,
                                                    @RequestParam ImageCategory imageCategory) {//멤버의 경우 필요가 없으니 -1을 부탁한다.
         return ResponseEntity.ok(awsS3Service.uploadSignupFile(multipartFile,email,imageCategory));
     }
 
-    @GetMapping("/public/image")
+    @GetMapping("/public/image")//이미지 조회
     public ResponseEntity<String> getFile(@RequestParam Long id,
                                           @RequestParam ImageCategory imageCategory) {//멤버의 경우 필요가 없으니 -1을 부탁한다.
         return ResponseEntity.ok(awsS3Service.getFile(id,imageCategory));
@@ -42,7 +42,7 @@ public class AmazonS3Conroller {
      * Amazon S3에 업로드 된 파일을 삭제
      * @return 성공 시 200 Success
      */
-    @DeleteMapping("/image")
+    @DeleteMapping("/image")//이미지 삭제
     public ResponseEntity<Void> deleteFile(@RequestParam Long id,
                                            @RequestParam ImageCategory imageCategory) {
         awsS3Service.deleteFile(id,imageCategory);
